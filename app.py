7c10b2257a5d0551a357d2f1a2c3829235bb3046
kiru6a <128159861+kiru6a@users.noreply.github.com>
2024-01-29 12:16:59 +0000

    create the login page and fixed password hashing

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, flash, redirect
-from database import SignupForm, insertUserIntoDb
-from flask_bcrypt import generate_password_hash
+from database import SignupForm, insertUserIntoDb, LoginForm, fetchUserByUsername
+from flask_bcrypt import generate_password_hash, check_password_hash, bcrypt
 
 
 app = Flask(__name__)
@@ -20,7 +20,10 @@ def trips():
 def signup():
   form = SignupForm()
   if form.validate_on_submit():
-    hashedPassword = str(generate_password_hash(form.password.data))
+    plainPassword = form.password.data
+    pwhash = bcrypt.hashpw(plainPassword.encode('utf8'), bcrypt.gensalt())
+    
+    hashedPassword = pwhash.decode()
     
     insertedId = insertUserIntoDb(form.username.data, hashedPassword)
     flash(f"User successfully registered with ID: {insertedId}", "success")
@@ -34,9 +37,25 @@ def tripsForUser(userId):
   return f"TODO for id {userId}"
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-  return "TODO"
+  form = LoginForm()
+  if form.validate_on_submit():
+    inputUsername = form.username.data
+    inputPassword = form.password.data
+    userRecord = fetchUserByUsername(form.username.data)
+    if not userRecord:
+      flash("Invalid username", "error")
+      return render_template("login.html", form=form)
+      
+    if not check_password_hash(userRecord.password, inputPassword):
+      flash("Invalid password", "error")
+      return render_template("login.html", form=form)
+      
+    flash(f"Welcome back, {userRecord.username}", "success")
+    return redirect(f"trips/{userRecord.user_id}")
+    
+  return render_template("login.html", form=form)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,14 +1,11 @@
 from sqlalchemy import create_engine, text
-from configparser import ConfigParser
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
+import os
 
 
-config = ConfigParser()
-config.read("config.properties")
-
-dbConnectionString = config.get("DEFAULT", "DB_CONNECTION_STRING")
+dbConnectionString = os.environ["DB_CONNECTION_STRING"]
 
 engine = create_engine(dbConnectionString)
 
@@ -22,13 +19,25 @@ class SignupForm(FlaskForm):
                            render_kw={"placeholder": "password"})
   submit = SubmitField(label="Signup")
 
-  def validate_username(self, username):
+  def validate_username(self, username: str):
     with engine.connect() as conn:
       stmt = text("SELECT username FROM planner_user "\
                   "WHERE username = :username")
       result = conn.execute(stmt, {"username": username.data})
       if result.fetchone():
         raise ValidationError("Username already exists. Please choose another one")
+
+
+class LoginForm(FlaskForm):
+  username = StringField(label="Username", validators=[InputRequired(),\
+              Length(min=4,max=20)], \
+              render_kw={"placeholder": "username"})
+  password = PasswordField(label="Password", validators=[InputRequired(),\
+                      Length(min=4, max=20)],\
+                           render_kw={"placeholder": "password"})
+  submit = SubmitField(label="Login")
+
+
 
 def insertUserIntoDb(username: str, password: str):
   with engine.connect() as conn:
@@ -39,4 +48,11 @@ def insertUserIntoDb(username: str, password: str):
     conn.commit()
     return result.lastrowid
 
+def fetchUserByUsername(username: str):
+  with engine.connect() as conn:
+    stmt = text("SELECT user_id, username, password "\
+               "FROM planner_user "\
+                "WHERE username = :username")
+    result = conn.execute(stmt, {"username": username})
+    return result.fetchone()
   
